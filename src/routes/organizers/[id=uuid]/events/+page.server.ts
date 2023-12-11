ae89555edbd9ef5e42288e37357efd49b65ef7ea
Pisti <pasztoristvan62@gmail.com>
2023-12-11 09:45:47 +0100

    feat: add event list backend

---
+++ src/routes/organizers/[id=uuid]/events/+page.server.ts
@@ -0,0 +1,14 @@
+import type { PageServerLoad } from './$types';
+import e from '@/edgeql-js';
+import { client } from '@/services/edgedb';
+
+export const load = (async ({ params }) => {
+  return {
+    events: await e
+      .select(e.Organizer.events, () => ({
+        ...e.Event['*'],
+        filter_single: { id: e.uuid(params.id) },
+      }))
+      .run(client),
+  };
+}) satisfies PageServerLoad;
