e5fa61db687bcac63d548d9388ac37e15d477c63
danieltakacs1 <dani.takacs2002@gmail.com>
2023-12-13 17:04:36 +0100

    fix: appbar not full length

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,38 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-
-	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
-	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
-	let currentTile: number = 0;
 	import { LightSwitch } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	let currentTile: number = 0;
 </script>
-<!-- App Shell -->
-<body data-theme="rocket">
-	<AppShell>
-		<svelte:fragment slot="header">
-			<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-				<span class="h1" slot="lead">Eventhost</span>
-				<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
-				
-			</AppBar>
-		</svelte:fragment>
-		<svelte:fragment slot="sidebarLeft">
-			<AppRail>
-				<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
-					<svelte:fragment slot="lead"></svelte:fragment>
-					<span class="text-xl">Main Page</span>
-				</AppRailTile>
-				<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
-					<svelte:fragment slot="lead"></svelte:fragment>
-					<span class="text-xl">Events</span>
-				</AppRailTile>
-				<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
-					<svelte:fragment slot="lead"></svelte:fragment>
-					<span class="text-xl">My Tickets</span>
-				</AppRailTile>
-			</AppRail>
-		</svelte:fragment>
-		<slot />
-	</AppShell>
-</body>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<span class="h1" slot="lead">Eventhost</span>
+			<svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+			
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail>
+			<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+				<svelte:fragment slot="lead"></svelte:fragment>
+				<span class="text-xl">Main Page</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+				<svelte:fragment slot="lead"></svelte:fragment>
+				<span class="text-xl">Events</span>
+			</AppRailTile>
+			<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+				<svelte:fragment slot="lead"></svelte:fragment>
+				<span class="text-xl">My Tickets</span>
+			</AppRailTile>
+		</AppRail>
+	</svelte:fragment>
+	<slot />
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,15 @@
-<body data-theme="rocket">
-	<div class="card">
-		<header class="card-header">
-			<img src="/src/image/img1.png" class="bg-black/50 w-full aspect-[21/9]" alt="img1"/>
-		</header>
-		<section class="p-4">
-			ISTVAN EGY BIMBIMBABMBIMBUM
-		</section>
-		<footer class="card-footer">
-			ISTVAN EGY KAKALAKASAKAMAKA
-		</footer>
-	</div>
-</body>
+<div class="card">
+	<header class="card-header">
+		<img src="/src/image/img1.png" class="bg-black/50 w-full aspect-[21/9]" alt="img1"/>
+	</header>
+	<section class="p-4">
+		ISTVAN EGY BIMBIMBABMBIMBUM
+	</section>
+	<footer class="card-footer">
+		ISTVAN EGY KAKALAKASAKAMAKA
+	</footer>
+</div>
+
 <script lang="ts">
 	let img1='$lib/pics/img1.png'
 </script>
