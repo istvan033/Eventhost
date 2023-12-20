f9d965985443e05184c4ae2359853ef1f04f6b92
Pisti <pasztoristvan62@gmail.com>
2023-12-20 11:02:18 +0100

    fix: ticket load

--- src/routes/tickets/[id=uuid]/+page.server.ts
+++ src/routes/tickets/[id=uuid]/+page.server.ts
@@ -4,11 +4,12 @@ import { client } from '@/services/edgedb';
 
 export const load = (async ({ params }) => {
   return {
-    events: await e
+    tickets: await e
       .select(e.Ticket, () => ({
         ...e.Ticket['*'],
         filter_single: { id: e.uuid(params.id) },
       }))
-      .run(client),
+      .run(client)
+      
   };
 }) satisfies PageServerLoad;
