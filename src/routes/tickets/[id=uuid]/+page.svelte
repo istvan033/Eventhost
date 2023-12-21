073e4202d702885963a1f7d3be5a957a66416a43
danieltakacs1 <dani.takacs2002@gmail.com>
2023-12-21 15:17:38 +0100

    ui: ticket page image

--- src/routes/tickets/[id=uuid]/+page.svelte
+++ src/routes/tickets/[id=uuid]/+page.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    
-    export let data: PageData;
+  import type { PageData } from './$types';
+  
+  export let data: PageData;
 </script>
 
+<div class="card border-2 border-surface-900 max-w-7xl max-h-fit variant-glass-surface mt-28 mx-auto">
+  <div class="overflow-hidden">
+    <img src="/src/routes/tickets/huh_cat.jpg" class="max-h-60 w-full"/>
+  </div>
+  
+  <div class="p-10 max-w-sm mx-auto justify-center">
 
-<div class="flex-grow -h-[460px] bg-white p-4 rounded shadow-md flex flex-col items-center justify-center">
-  <div class="max-w flex flex-col">
-    <p class="text-center mb-4 text-3xl mt-0">RENDEZVENY NEVaminagyonhosszu</p>
-    
+    <p class="text-center mb-4 text-3xl mt-0">organizationName</p>
+      
     <p class="mb-0 text-left font-medium text-sm">CÉGNÉV</p>
     <p class="mb-4 text-2xl">{data.tickets?.organizationName}</p>
     
     <p class="mb-0 text-left font-medium text-sm">NÉV</p>
     <p class="mb-4 text-2xl">{data.tickets?.firstName} {data.tickets?.lastName} </p>
-    <!-- svelte-ignore a11y-missing-attribute -->
+    
     <img
       src="/src/routes/tickets/[id=uuid]/pic/qrcode.png"
       width="225"
@@ -23,4 +27,4 @@
     <p class="text-center mb-4">{data.tickets?.id}</p>
   </div>
 </div>
-
+<!-- test-->
