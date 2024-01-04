95c1d811e909542ed7dbbfa5e020e50467152922
Pisti <pasztoristvan62@gmail.com>
2024-01-04 07:58:45 +0100

    feat: add event load admin

---
+++ src/routes/events/[id=uuid]/tickets/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+  </script>
+
+<div>
+    
+      address
+      <p>{data.Event?.id}</p>
+      {data.Event?.address}
+      {data.Event?.addressDetail}
+      {data.Event?.zipCode}
+      {data.Event?.createdAt}
+      {data.Event?.updatedAt}@@   
+      {data.Event?.endsAt}
+      {data.Event?.startsAt}
+      {data.Event?.description}
+      {data.Event?.placeName}
+      {data.Event?.title}
+      
+    
+</div>
+
