c2cbea540f7187ee789398847d1c713bfea2e8b3
Pisti <pasztoristvan62@gmail.com>
2024-02-23 09:57:42 +0100

    feat: about us page

---
+++ src/routes/(app)/aboutus/+page.svelte
@@ -0,0 +1,69 @@
+<div class="flex flex-col items-center mt-12">
+  <h2 class="text-2xl font-bold">Eventhost Team</h2>
+</div>
+
+  <div class="flex justify-between">
+    <div class="card border-2 border-surface-900 max-w-7xl max-h-fit variant-glass-surface mt-10 mx-auto rounded-3xl  mb-8">
+      <a class="card card-hover overflow-hidden" href="https://github.com/istvan033">
+        <header>
+          <img src="src/routes/(app)/image/profil.jpg" class="p-10 max-w-sm justify-center rounded-full" alt="kep" />
+        </header>
+        <div class="p-10 max-w-sm mx-auto justify-center space-y-4">
+          <p class="h6 text-s">Backend Developer:</p>
+          <h3 class="h3">Pásztor István</h3>
+          <article>
+            <p>
+              <!-- cspell:disable -->
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet eligendi cumque
+              consequatur tempore sint nisi sapiente. Iste beatae laboriosam iure molestias cum expedita architecto itaque quae
+              rem.
+              <!-- cspell:enable -->
+            </p>
+          </article>
+        </div>
+      </a>
+    </div>
+
+    <div class="card border-2 border-surface-900 max-w-7xl max-h-fit variant-glass-surface mt-10 mx-auto rounded-3xl  mb-8">
+      <a class="card card-hover overflow-hidden" href="https:/github.com/danieltakacs1">
+        <header>
+          <img src="src/routes/(app)/image/profil2.jpg" class="p-10 max-w-sm justify-center rounded-full" alt="kep" />
+        </header>
+        <div class="p-10 max-w-sm mx-auto justify-center space-y-4">
+          <p class="h6 text-s">Frontend developer:</p>
+          <h3 class="h3" data-toc-ignore>Takács Dániel</h3>
+          <article>
+            <p>
+              <!-- cspell:disable -->
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet eligendi cumque
+              consequatur tempore sint nisi sapiente. Iste beatae laboriosam iure molestias cum expedita architecto itaque quae
+              rem.
+              <!-- cspell:enable -->
+            </p>
+          </article>
+        </div>
+      </a>
+    </div>
+
+    <div class="card border-2 border-surface-900 max-w-7xl max-h-fit variant-glass-surface mt-10 mx-auto rounded-3xl  mb-8">
+      <a class="card card-hover overflow-hidden" href="https:/github.com/danieltakacs1">
+        <header>
+          <img src="src/routes/(app)/image/profil3.jpg" class="p-10 max-w-sm justify-center rounded-full" alt="kep" />
+        </header>
+        <div class="p-10 max-w-sm mx-auto justify-center space-y-4">
+          <p class="h6 text-s">Designe , Documentation</p>
+          <h3 class="h3" data-toc-ignore>Szeri Bendegúz</h3>
+          <article>
+            <p>
+              <!-- cspell:disable -->
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet eligendi cumque
+              consequatur tempore sint nisi sapiente. Iste beatae laboriosam iure molestias cum expedita architecto itaque quae
+              rem.
+              <!-- cspell:enable -->
+            </p>
+          </article>
+        </div>
+      </a>
+    </div>
+
+  </div>
