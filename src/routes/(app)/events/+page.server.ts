fa4d3dc9cb8eb916971a9ac92e45c2dfe74feae5
Takács Dániel <dani.takacs2002@gmail.com>
2024-03-06 11:10:05 +0100

    add: select user events

--- src/routes/(app)/events/+page.server.ts
+++ src/routes/(app)/events/+page.server.ts
@@ -7,22 +7,27 @@ import type { RequestEvent } from "./$types"
 
 export const load = (async ({ locals }: RequestEvent) => {
   const session = await locals.auth()
-  if (!session?.user) {
-    throw redirect(307, '/');
-  }
-  else {
+  if (session?.user) {
+    const sessionEmail = session?.user?.email;
+    const afterAt = sessionEmail?.split('@')[1];
+    const result: string = afterAt as string;
     return {
       event: await e
-        .select(e.Event, () => ({
+        .select(e.Event, (event) => ({
           id: true,
           title: true,
           placeName: true,
           startsAt: true,
           endsAt: true,
           organizer: {firstName: true, lastName: true},
+          emailValidation: true,
+          filter: e.op(event.emailValidation, '=', result)
         }))
         .run(client),
     };
+  }
+  else {
+    throw redirect(307, '/');
   }
 }) satisfies PageServerLoad;
 
